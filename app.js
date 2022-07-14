1a9bf40c884e526b38f00a8b2a0f857ec595b8aa
Abigail Gilson <abigailwil95@gmail.com>
2022-07-14 11:58:55 -0700

    now works locally

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import Body from "./components/Body"
-import Footer from "./components/Footer"
-import Header from "./components/Header"
+    import React from "react"
+    import ReactDOM from "react-dom"
+    import Body from "./components/Body"
+    import Footer from "./src/components/Footer"
+    import Header from "./src/components/Header"
 
-export default function app(){
-    return(
-        <div className="app">
-            <Header />
-            <Body />
-            <Footer />
-        </div>
-    )    
-}
+    export default function app(){
+        return(
+            <div className="app">
+                <Header />
+                <Body />
+                <Footer />
+            </div>
+        )    
+    }
--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import ReactDOM from "react-dom"
+import Body from "./components/Body"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function app(){
+    return(
+        <div className="app">
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+    )    
 }
-
-export default App;
