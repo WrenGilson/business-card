846c5f22cef8338a22736950099a46fffcc733a2
Abigail Gilson <abigailwil95@gmail.com>
2022-07-16 09:47:30 -0700

    update

--- docs/App.js
+++ docs/App.js
@@ -1,5 +1,7 @@
-import React from "react"
-import ReactDOM from "react-dom"
+<script crossorigin src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
+<script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.24.0/babel.js"></script>
+
 import Body from "./components/Body"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
