4f6566564f5ba3eb5c7e60894d907ba6521c375a
Abigail Gilson <abigailwil95@gmail.com>
2022-07-17 10:10:03 -0700

    package.json

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,9 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
+define(function(require, exports, module) {
+  //Put traditional CommonJS module content here
+});
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
