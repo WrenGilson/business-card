d1d2f74e083835c731ebe6b6bef77873bc503d13
Abigail Gilson <abigailwil95@gmail.com>
2022-08-10 15:30:36 -0700

    build

---
+++ build/precache-manifest.4effa30372b01f9f194fe888f3531aaa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "eaed9fb1b97a297892e4",
+    "url": "/business-card/static/css/main.a2358c3c.chunk.css"
+  },
+  {
+    "revision": "eaed9fb1b97a297892e4",
+    "url": "/business-card/static/js/main.eaed9fb1.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/business-card/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "0f07bc3e528eeaf61b3f",
+    "url": "/business-card/static/js/2.0f07bc3e.chunk.js"
+  },
+  {
+    "revision": "9da041aaac97c4801bdd",
+    "url": "/business-card/static/js/runtime~main.9da041aa.js"
+  },
+  {
+    "revision": "bd8d33454102882c47198f750622ee3c",
+    "url": "/business-card/static/media/email.bd8d3345.png"
+  },
+  {
+    "revision": "5544e967446f79d66ab095a41b0fffd7",
+    "url": "/business-card/static/media/linkedin.5544e967.png"
+  },
+  {
+    "revision": "76997491439a6e202bbbd9bc3be72ca3",
+    "url": "/business-card/static/media/wren.76997491.jpg"
+  },
+  {
+    "revision": "59c4037b2b2065bbdbb96d3e2e3a34ab",
+    "url": "/business-card/index.html"
+  }
+];
